2ba82d3907b3e70b223f0c35ecb4fd03fd6e7771
lucascesarnf <lucascesarnf@hotmail.com>
2017-07-03 15:26:53 -0300

    Initial Commit

---
+++ JogoDaVelhaTests/JogoDaVelhaTests.swift
@@ -0,0 +1,36 @@
+//
+//  JogoDaVelhaTests.swift
+//  JogoDaVelhaTests
+//
+//  Created by Lucas César  Nogueira Fonseca on 03/07/17.
+//  Copyright © 2017 Lucas César  Nogueira Fonseca. All rights reserved.
+//
+
+import XCTest
+@testable import JogoDaVelha
+
+class JogoDaVelhaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
